f5bf2571b552e134903bf5ebdb71bc9cd65b4fbe
David Fellows <dfellows12@gmail.com>
2020-01-28 15:04:42 -0500

    Built out components

---
+++ src/components/SongList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SongList = () => {
+    return ( 
+        <div className={"song-list"}>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default SongList;
